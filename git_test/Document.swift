ccb53742937879a6e7687f7ce48882a7da22465f
hye0njun <hhj56230@gmail.com>
2024-01-22 16:28:57 +0900

    Initial Commit

---
+++ git_test/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  git_test
+//
+//  Created by 황현준 on 2024/01/22.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
